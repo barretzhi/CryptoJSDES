88911cf5820c1188e2a9d64920f74239273f30af
Barret.zhi <barret.zhi@gmail.com>
2022-05-13 15:44:07 +0800

    Create CryptoJSDES.php
    提交第一个文件

---
+++ CryptoJSDES.php
@@ -0,0 +1,63 @@
+<?php
+
+class CryptoJSDES {
+
+    /**
+     * @param      $data
+     * @param      $passphrase
+     * @param null $salt        ONLY FOR TESTING
+     * @return string           encrypted data in base64 OpenSSL format
+     */
+    public static function encrypt($data, $passphrase, $salt = null) {
+        $salt = $salt ?: openssl_random_pseudo_bytes(8);
+        list($key, $iv) = self::evpkdf($passphrase, $salt);
+
+        $ct = openssl_encrypt($data, 'des-cbc', $key, true, $iv);
+
+        return self::encode($ct, $salt);
+    }
+
+    /**
+     * @param string $base64        encrypted data in base64 OpenSSL format
+     * @param string $passphrase
+     * @return string
+     */
+    public static function decrypt($base64, $passphrase) {
+        list($ct, $salt) = self::decode($base64);
+        list($key, $iv) = self::evpkdf($passphrase, $salt);
+
+        $data = openssl_decrypt($ct, 'des-cbc', $key, true, $iv);
+
+        return $data;
+    }
+
+    public static function evpkdf($passphrase, $salt) {
+        $salted = '';
+        $dx = '';
+        while (strlen($salted) < 48) {
+            $dx = md5($dx . $passphrase . $salt, true);
+            $salted .= $dx;
+        }
+        $key = substr($salted, 0, 8);   //AES将length:8改成32
+        $iv = substr($salted, 8, 8);    //AES将offset:8改成32,length:8改成16
+
+        return [$key, $iv];
+    }
+
+    public static function decode($base64) {
+        $data = base64_decode($base64);
+
+        if (substr($data, 0, 8) !== "Salted__") {
+            throw new \InvalidArgumentException();
+        }
+
+        $salt = substr($data, 8, 8);
+        $ct = substr($data, 16);
+
+        return [$ct, $salt];
+    }
+
+    public static function encode($ct, $salt) {
+        return base64_encode("Salted__" . $salt . $ct);
+    }
+}
